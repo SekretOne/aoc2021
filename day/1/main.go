cafe5391d31a2aae96d5eae22e89df7156791054
Nathan Little <sekretone@gmail.com>
2021-12-05 11:39:05 -0500

    day:1.2 solved

--- day/1/main.go
+++ day/1/main.go
@@ -48,9 +48,37 @@ func totalIncreases(metrics []int) int {
 	return total
 }
 
+// sum all the integers in the slice
+func sum(addends []int) int {
+	s := 0
+	for _, val := range addends {
+		s += val
+	}
+	return s
+}
+
+// count the number of times a depth measurement increases in a rolling window
+// context: https://adventofcode.com/2021/day/1
+func totalIncreasesInWindow(metrics []int, window int) int {
+	var total = 0
+	for i := 0; i < len(metrics)-window; i++ {
+		a := sum(metrics[i : i+window])
+		b := sum(metrics[i+1 : i+window+1])
+		if a < b {
+			total++
+		}
+	}
+	return total
+}
+
 func main() {
 	metrics := readInput("day/1/input.txt")
 	total := totalIncreases(metrics)
 
+	println("Total increases (window size of 1)")
+	println(total)
+
+	println("Total increases (window size of 3")
+	total = totalIncreasesInWindow(metrics, 3)
 	println(total)
 }
