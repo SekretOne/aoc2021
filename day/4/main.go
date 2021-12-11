bde4057a8e3311a236262727e69c409169c158ef
Nathan Little <sekretone@gmail.com>
2021-12-10 21:56:18 -0500

    day:4.1 solved

--- day/4/main.go
+++ day/4/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 const (
 	rowWin = int(0b0000000000000000000011111)
 	colWin = int(0b0000100001000010000100001)
@@ -30,7 +39,7 @@ func (b *bingo) hasWon() bool {
 	return false
 }
 
-// marks numbers
+// marks numbers and returns the index of the space that was marked (or -1 if not) and whether this was winning
 func (b *bingo) mark(number int) (spaceIndex int, isWinner bool) {
 	for i, space := range b.spaces {
 		if number == space {
@@ -41,11 +50,85 @@ func (b *bingo) mark(number int) (spaceIndex int, isWinner bool) {
 	return -1, false
 }
 
+// scores the bingo game (sum of unused numbers times the winning number)
+func score(b *bingo, winningNumber int) int {
+	sumOfUnmarked := 0
+	for i := 0; i < 25; i++ {
+		if 1<<i&b.marks == 0 { //if unmarked
+			sumOfUnmarked += b.spaces[i]
+		}
+	}
+	return winningNumber * sumOfUnmarked
+}
+
+func readInput(fileName string) (numbers []int, bingos []*bingo) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	var rawNumbers string
+	if _, err := fmt.Fscanln(file, &rawNumbers); err != nil {
+		log.Fatalln(err)
+	}
+	splits := strings.Split(rawNumbers, ",")
+	numbers = make([]int, 0, len(splits))
+
+	for _, s := range splits {
+		n, err := strconv.Atoi(s)
+		if err != nil {
+			log.Fatalf("unable to convert into number array, value: %v\n", s)
+		}
+		numbers = append(numbers, n)
+	}
+
+	bingos = make([]*bingo, 0, 4)
+
+	for {
+		b := bingo{
+			spaces: [25]int{},
+			marks:  0,
+		}
+		// read the 25 spaces
+		if _, err := fmt.Fscanln(file); err != nil {
+			log.Fatalf("unexpected format; is not a bingo card")
+		}
+
+		for i := 0; i < 25; i += 5 {
+			if _, err := fmt.Fscanln(file, &b.spaces[i], &b.spaces[i+1], &b.spaces[i+2], &b.spaces[i+3], &b.spaces[i+4]); err != nil {
+				if err == io.EOF {
+					return numbers, bingos
+				}
+				log.Fatalf("unable to read number %v: %v\n", i, err)
+			}
+		}
+		bingos = append(bingos, &b)
+	}
+}
+
 type bingo struct {
 	spaces [25]int
 	marks  int
 }
 
 func main() {
+	numbers, cards := readInput("day/4/input.txt")
 
+	for _, n := range numbers {
+		for _, card := range cards {
+			if _, won := card.mark(n); won {
+
+				s := score(card, n)
+				fmt.Printf("card: {\n  %v,\n%025b,\n}\n", card.spaces, card.marks)
+				fmt.Printf("winning score: %v\n", s)
+				return
+			}
+		}
+	}
+	fmt.Println("Nothing won?")
 }
